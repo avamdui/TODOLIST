9696ec05de6863c70b06aa71a54d51d9038b698f
avamdui <54531433+avamdui@users.noreply.github.com>
2022-03-01 16:45:48 +0100

    départ TT

--- src/Controller/Admin/TaskCrudController.php
+++ src/Controller/Admin/TaskCrudController.php
@@ -10,9 +10,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 
 class TaskCrudController extends AbstractCrudController
 {
+    private $security;
+
     public static function getEntityFqcn(): string
     {
         return Task::class;
@@ -21,13 +24,18 @@ class TaskCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+
+        $user = $this->security->getUser();
+
         yield IdField::new('id')
             ->onlyOnIndex();
         yield TextField::new('title');
         yield TextareaField::new('content')
             ->onlyOnForms();
-        yield BooleanField::new('is_done')
-            ->renderAsSwitch(false);
+        if ($this->getUser() == $user) {
+            yield BooleanField::new('is_done')
+                ->renderAsSwitch(false);
+        }
         yield DateField::new('createdAt')
             ->hideOnForm();
     }
