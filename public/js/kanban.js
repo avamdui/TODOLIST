0b27c75c4c39014cae74dc0e1431f07b6117cbaa
avamdui <54531433+avamdui@users.noreply.github.com>
2022-03-02 20:38:17 +0100

    drag and drop

--- public/js/kanban.js
+++ public/js/kanban.js
@@ -1,19 +1,59 @@
+// $(document).ready(function () {
+
+//     $('#btnToggleTask').on('click', function () {
+//         alert('coucou');
+//         let id = $(this).attr('data-task-id');
+//         // $.getJSON('/tasks/' + id + '/toggle').then(function (rep) { alert(rep); });
+//         $(this).load('/tasks/' + id + '/toggle');
+//     });
+
+//     $('#btnDeleteTask').on('click', function () {
+//         let id = $(this).attr('data-task-id');
+//         $.getJSON('/tasks/' + id + '/delete')
+//             .then(function (rep) {
+//                 if (rep == 'ok') {
+//                     $('#task-' + id).remove();
+//                 }
+//             });
+//     });
+
+// });
 $(document).ready(function () {
 
-    $('#btnToggleTask').on('click', function () {
-        alert('coucou');
-        let id = $(this).attr('data-task-id');
-        // $.getJSON('/tasks/' + id + '/toggle').then(function (rep) { alert(rep); });
-        $(this).load('/tasks/' + id + '/toggle');
+    // There's the gallery and the trash
+    var $todo = $("#todo"),
+        $done = $("#done"),
+        $trash = $("#trash");
+
+    // Let the gallery items be draggable
+    $("li", $todo).draggable({ revert: "invalid" });
+    $("li", $done).draggable({ revert: "invalid" });
+    $($trash).draggable({ revert: "invalid" });
+
+    // Let the trash be droppable, accepting the gallery items
+    $trash.droppable(
+        {
+            drop: function (event, ui) {
+                let id = $(this).attr('data-task-id');
+                $.getJSON('/tasks/' + id + '/delete')
+                    .then(function (rep) {
+                        if (rep == 'ok') {
+                            $('#task-' + id).remove();
+                        }
+                    });
+            }
+        });
+
+
+    // Let the gallery be droppable as well, accepting items from the trash
+    $todo.droppable({
+        accept: "#trash",
+        classes: {
+            "ui-droppable-active": "custom-state-active"
+        },
+        drop: function (event, ui) {
+            recycleImage(ui.draggable);
+        }
     });
 
-    $('#btnDeleteTask').on('click', function () {
-        let id = $(this).attr('data-task-id');
-        $.getJSON('/tasks/' + id + '/delete')
-            .then(function (rep) {
-                if (rep == 'ok') {
-                    $('#task-' + id).remove();
-                }
-            });
-    });
 });
