938d33feb4049507a85cdf8063379fe095da6d1d
avamdui <54531433+avamdui@users.noreply.github.com>
2022-03-24 15:33:41 +0100

    TEST composer unused

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '20bdb659894e017179dcc7b7d9d70035e8168599',
+        'reference' => '34eef9553ee4d9eb8daad7b2c9d322cce415a924',
         'name' => '__root__',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '20bdb659894e017179dcc7b7d9d70035e8168599',
+            'reference' => '34eef9553ee4d9eb8daad7b2c9d322cce415a924',
             'dev_requirement' => false,
         ),
         'doctrine/annotations' => array(
